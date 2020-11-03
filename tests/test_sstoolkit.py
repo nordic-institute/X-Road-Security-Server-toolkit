9813cdb5d36aab93802a287c0cd6ccd6c4457726
Taimo Peelo <unserializable@timedrops.us>
2020-11-03 17:26:20 +0200

    Fix test of failed command 'command1'.

--- tests/test_sstoolkit.py
+++ tests/test_sstoolkit.py
@@ -21,16 +21,5 @@ def test_command1():
     # test command1 without arguments
     argv = ['command1']
     with SSToolkitTest(argv=argv) as app:
-        app.run()
-        data,output = app.last_rendered
-        assert data['foo'] == 'bar'
-        assert output.find('Foo => bar')
-
-
-    # test command1 with arguments
-    argv = ['command1', '--foo', 'not-bar']
-    with SSToolkitTest(argv=argv) as app:
-        app.run()
-        data,output = app.last_rendered
-        assert data['foo'] == 'not-bar'
-        assert output.find('Foo => not-bar')
+        with raises(SystemExit, match="[^0]"):
+            app.run()
