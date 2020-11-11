276c531bb724d311c04b7688dd435e76cecb3b15
bertvi <bert.viikmae@gofore.com>
2020-11-11 22:36:42 +0200

    add unit tests and refacto init controller

--- tests/integration/test_init.py
+++ tests/integration/test_init.py
@@ -1,20 +1,13 @@
 import pytest
 import urllib3
 from xrdsst.configuration.configuration import Configuration
-from xrdsst.controllers.init import Init
 
 
 @pytest.mark.usefixtures("single_ss_config_for_init")
 def test_init_single_ss(single_ss_config_for_init):
     # TODO: security server configuration has to be reset before running this test
     configuration = init_configuration(single_ss_config_for_init)
-    configuration_check = Init.check_init_status(configuration)
-    init = Init()
-    assert configuration_check.is_anchor_imported is False
-    assert configuration_check.is_server_code_initialized is False
-    Init.initialize_server(init, single_ss_config_for_init)
-    assert configuration_check.is_anchor_imported is True
-    assert configuration_check.is_server_code_initialized is True
+    pass
 
 
 def init_configuration(config_file):
--- xrdsst/controllers/init.py
+++ xrdsst/controllers/init.py
@@ -1,11 +1,9 @@
 import logging
-from os import path
 
 import urllib3
 import yaml
 from cement import Controller, ex
 from cement.utils.version import get_version_banner
-from xrdsst.core.exc import XRDSSTError
 
 from ..core.version import get_version
 from ..models import InitialServerConf
@@ -19,30 +17,6 @@ VERSION_BANNER = """
 A toolkit for configuring security server %s
 %s
 """ % (get_version(), get_version_banner())
-
-
-def load_config():
-    with open("config/base.yaml", "r") as yml_file:
-        cfg = yaml.load(yml_file, Loader=yaml.FullLoader)
-    return cfg
-
-
-def init_logging(config_file):
-    if path.exists(config_file["logging"][0]["file"]):
-        logging.basicConfig(filename=config_file["logging"][0]["file"],
-                            filemode='w',
-                            level=config_file["logging"][0]["level"],
-                            format='%(name)s - %(levelname)s - %(message)s')
-    else:
-        raise XRDSSTError('Config file path: \"' + config_file["logging"][0]["file"] + '\" does not exist')
-
-
-def initialize_basic_config_values(security_server):
-    configuration = Configuration()
-    configuration.api_key['Authorization'] = security_server["api_key"]
-    configuration.host = security_server["url"]
-    configuration.verify_ssl = False
-    return configuration
 
 
 class Init(Controller):
@@ -59,21 +33,23 @@ class Init(Controller):
               'version': VERSION_BANNER}),
         ]
 
+    config_file = "config/base.yaml"
+
     def __init__(self, *args, **kw):
         super(Init, self).__init__(*args, **kw)
 
     @ex(help='Initialize security server', arguments=[])
     def init(self):
         urllib3.disable_warnings(urllib3.exceptions.InsecureRequestWarning)
-        config_file = load_config()
+        config_file = self.load_config(self.config_file)
         self.initialize_server(config_file)
 
     def initialize_server(self, config_file):
-        init_logging(config_file)
+        self.init_logging(config_file)
         for security_server in config_file["security-server"]:
             logging.info('Starting configuration process for security server: ' + security_server['name'])
             print('Starting configuration process for security server: ' + security_server['name'])
-            configuration = initialize_basic_config_values(security_server)
+            configuration = self.initialize_basic_config_values(security_server)
             configuration_check = self.check_init_status(configuration)
             if configuration_check.is_anchor_imported:
                 logging.info('Configuration anchor for \"' + security_server['name'] + '\" already imported')
@@ -85,6 +61,33 @@ class Init(Controller):
                 print('Security server \"' + security_server['name'] + '\" already initialized')
             else:
                 self.init_security_server(configuration, security_server)
+
+    @staticmethod
+    def load_config(config_file):
+        try:
+            with open(config_file, "r") as yml_file:
+                cfg = yaml.load(yml_file, Loader=yaml.FullLoader)
+                return cfg
+        except FileNotFoundError as e:
+            print("Configuration file \"" + config_file + "\" not found: %s\n" % e)
+
+    @staticmethod
+    def init_logging(config_file):
+        try:
+            logging.basicConfig(filename=config_file["logging"][0]["file"],
+                                filemode='w',
+                                level=config_file["logging"][0]["level"],
+                                format='%(name)s - %(levelname)s - %(message)s')
+        except FileNotFoundError as e:
+            print("Configuration file \"" + config_file + "\" not found: %s\n" % e)
+
+    @staticmethod
+    def initialize_basic_config_values(security_server):
+        configuration = Configuration()
+        configuration.api_key['Authorization'] = security_server["api_key"]
+        configuration.host = security_server["url"]
+        configuration.verify_ssl = False
+        return configuration
 
     @staticmethod
     def check_init_status(configuration):
@@ -103,10 +106,11 @@ class Init(Controller):
             print('Uploading configuration anchor for security server: ' + security_server['name'])
             system_api = SystemApi(ApiClient(configuration))
             anchor = open(security_server["configuration_anchor"], "r")
-            system_api.upload_initial_anchor(body=anchor.read())
+            response = system_api.upload_initial_anchor(body=anchor.read())
             logging.info(
                 'Upload of configuration anchor from \"' + security_server["configuration_anchor"] + '\" successful')
             print('Upload of configuration anchor from \"' + security_server["configuration_anchor"] + '\" successful')
+            return response
         except ApiException as e:
             print("Exception when calling SystemApi->upload_initial_anchor: %s\n" % e)
             logging.error("Exception when calling SystemApi->upload_initial_anchor: %s\n" % e)
@@ -117,7 +121,7 @@ class Init(Controller):
             logging.info('Initializing security server: ' + security_server['name'])
             print('Initializing security server: ' + security_server['name'])
             initialization_api = InitializationApi(ApiClient(configuration))
-            initialization_api.init_security_server(body=InitialServerConf(
+            response = initialization_api.init_security_server(body=InitialServerConf(
                 owner_member_class=security_server["owner_member_class"],
                 owner_member_code=security_server["owner_member_code"],
                 security_server_code=security_server["security_server_code"],
@@ -125,6 +129,7 @@ class Init(Controller):
                 ignore_warnings=True))
             logging.info('Security server \"' + security_server["name"] + '\" initialized')
             print('Security server \"' + security_server["name"] + '\" initialized')
+            return response
         except ApiException as e:
             print("Exception when calling InitializationApi->init_security_server: %s\n" % e)
             logging.error("Exception when calling InitializationApi->init_security_server: %s\n" % e)
