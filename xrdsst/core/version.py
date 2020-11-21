5a5b71dfcbb35ad626e344797a2d3b0dfc4b07d8
bertvi <bert.viikmae@gofore.com>
2020-11-21 10:08:49 +0200

    Bump version: "0.1.4" → 0.1.5-dev0

--- xrdsst/core/version.py
+++ xrdsst/core/version.py
@@ -1,7 +1,14 @@
 """Module for getting project version"""
 from cement.utils.version import get_version as cement_get_version
 
-VERSION = (0, 1, 4, 'alpha', 0)
+"0.1.5-dev0"
+
+
+def convert_version(current_version):
+    return 0, 1, 4, 'alpha', 0
+
+
+VERSION = convert_version(current_version)
 
 
 def get_version(version=VERSION):
