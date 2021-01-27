e99996f0832bb2bf0cb4fa843510921914840424
bertvi <bert.viikmae@gofore.com>
2021-01-27 16:10:02 +0200

    improve test coverage

--- tests/unit/test_service.py
+++ tests/unit/test_service.py
@@ -85,6 +85,29 @@ class TestService(unittest.TestCase):
                         sys.stdout.write(out)
                         sys.stderr.write(err)
 
+    def test_service_description_add_client_not_found(self):
+        class ClientNotFound:
+            status = 404
+            data = '{"status":404,"error":{"code":"service_description_client_not_foubd"}}'
+            reason = None
+
+            def getheaders(self): return None
+
+        with XRDSSTTest() as app:
+            with mock.patch('xrdsst.api.clients_api.ClientsApi.find_clients',
+                            side_effect=ApiException(http_resp=ClientNotFound())):
+                service_controller = ServiceController()
+                service_controller.app = app
+                service_controller.load_config = (lambda: self.ss_config)
+                service_controller.add_description()
+
+                out, err = self.capsys.readouterr()
+                assert out.count("ClientsApi->find_clients") > 0
+
+                with self.capsys.disabled():
+                    sys.stdout.write(out)
+                    sys.stderr.write(err)
+
     def test_service_description_add_already_existing(self):
         class AlreadyExistingResponse:
             status = 409
@@ -114,6 +137,40 @@ class TestService(unittest.TestCase):
 
                     out, err = self.capsys.readouterr()
                     assert out.count("already exists") > 0
+
+                    with self.capsys.disabled():
+                        sys.stdout.write(out)
+                        sys.stderr.write(err)
+
+    def test_service_description_add_other_exception(self):
+        class PermissionDeniedResponse:
+            status = 403
+            data = '{"status":409,"error":{"code":"service_description_permission_denied"}}'
+            reason = None
+
+            def getheaders(self): return None
+
+        with XRDSSTTest() as app:
+            with mock.patch('xrdsst.api.clients_api.ClientsApi.find_clients', return_value=[Client(
+                    id='DEV:GOV:9876:SUB1',
+                    instance_id='DEV',
+                    member_class='GOV',
+                    member_code='9876',
+                    subsystem_code='SUB1',
+                    connection_type=ConnectionType.HTTP,
+                    status=ClientStatus.REGISTERED,
+                    owner=True,
+                    has_valid_local_sign_cert=True
+            )]):
+                with mock.patch('xrdsst.api.clients_api.ClientsApi.add_client_service_description',
+                                side_effect=ApiException(http_resp=PermissionDeniedResponse())):
+                    service_controller = ServiceController()
+                    service_controller.app = app
+                    service_controller.load_config = (lambda: self.ss_config)
+                    service_controller.add_description()
+
+                    out, err = self.capsys.readouterr()
+                    assert out.count("ClientsApi->add_client_service_description") > 0
 
                     with self.capsys.disabled():
                         sys.stdout.write(out)
@@ -183,3 +240,23 @@ class TestService(unittest.TestCase):
                         with self.capsys.disabled():
                             sys.stdout.write(out)
                             sys.stderr.write(err)
+
+    def test_is_description_disabled(self):
+        with XRDSSTTest() as app:
+            with mock.patch('xrdsst.api.clients_api.ClientsApi.find_clients', return_value=[Client(
+                    id='DEV:GOV:9876:SUB1',
+                    instance_id='DEV',
+                    member_class='GOV',
+                    member_code='9876',
+                    subsystem_code='SUB1',
+                    connection_type=ConnectionType.HTTP,
+                    status=ClientStatus.REGISTERED,
+                    owner=True,
+                    has_valid_local_sign_cert=True
+            )]):
+                with mock.patch('xrdsst.api.clients_api.ClientsApi.get_client_service_descriptions',
+                                return_value=[ServiceTestData.add_description_response]):
+                    service_controller = ServiceController()
+                    service_controller.app = app
+                    service_controller.load_config = (lambda: self.ss_config)
+                    assert service_controller.is_description_disabled(self.ss_config)
