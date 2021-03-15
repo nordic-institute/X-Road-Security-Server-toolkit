1aabd4b114f85a8856a296c5d567b1be45407a18
bertvi <bert.viikmae@gofore.com>
2021-03-15 11:13:43 +0200

    refacto reset security server script to create SSH user

--- reset_security_server.sh
+++ reset_security_server.sh
@@ -1,17 +1,18 @@
 #!/bin/bash
-# Usage reset_security_server.sh -n server(s) -h path/to/hosts.txt -a path/to/ansible -k path/to/public_key_file
+# Usage reset_security_server.sh -n server(s) -h path/to/hosts.txt -a path/to/ansible -u ssh_user -k path/to/public_key_file
 #
 # Description of required command line arguments:
 #
 #   -n: name(s) of security servers to be re-initialized (has to conform with the ones listed in hosts file)
 #   -h: lxd hosts file, this file is used by the Ansible script to install security servers
 #   -a: path to ansible script, this is the path to where the Ansible script is located in the locally cloned X-Road Git repository
+#   -u: ssh user name
 #   -k: path to public ssh key file
 #
 #   The LXD hosts file and the Ansible script file that should be used by this script can be found here:
 #   https://github.com/nordic-institute/X-Road/blob/develop/ansible
 #
-# Usage example: reset_security_server.sh -n ss3,ss4 -h ../X-Road/ansible/hosts/lxd_hosts.txt -a ../X-Road/ansible -k public_key_file
+# Usage example: reset_security_server.sh -n ss3,ss4 -h ../X-Road/ansible/hosts/lxd_hosts.txt -a ../X-Road/ansible -u ssh_user -k public_key_file
 
 ANSIBLE_CMD="ansible-playbook"
 ANSIBLE_SCRIPT="xroad_init.yml"
@@ -19,7 +20,7 @@ SSH_FOLDER=".ssh"
 AUTHORIZED_KEYS_FILE="authorized_keys"
 
 usage() {
-  echo "Usage: reset_security_server.sh -n name(s) -h hosts.txt -a ansible_folder -k public_key_file"
+  echo "Usage: reset_security_server.sh -n name(s) -h hosts.txt -a ansible_folder -u ssh_user -k public_key_file"
 }
 
 exit_abnormal() {
@@ -64,12 +65,18 @@ add_public_key() {
     for name in $names
     do
         printf "\nAdding public key to LXD container %s\n" "$name"
-        lxc exec "$name" -- bash -c "mkdir -p $3"
-        lxc exec "$name" -- bash -c "echo \"$(cat "$2")\" > $3/$4"
+        lxc exec "$name" -- bash -c "useradd -c \"$2 user\" -d /home/$2 -s /bin/bash $2"
+        lxc exec "$name" -- bash -c "echo \"$2 ALL=(ALL) NOPASSWD: ALL\" > /etc/sudoers.d/$2"
+        lxc exec "$name" -- bash -c "mkdir -p /home/$2/$4"
+        lxc exec "$name" -- bash -c "echo \"$(cat "$3")\" > /home/$2/$4/$5"
+        lxc exec "$name" -- bash -c "chown -R $2:$2 /home/$2"
+        lxc exec "$name" -- bash -c "chmod 0700 /home/$2"
+        lxc exec "$name" -- bash -c "chmod 0700 /home/$2/$4"
+        lxc exec "$name" -- bash -c "chmod 0600 /home/$2/$4/$5"
     done
 }
 
-while getopts ":n:h:a:k:" options; do
+while getopts ":n:h:a:u:k:" options; do
   case "${options}" in
     n )
       NAME=${OPTARG}
@@ -79,6 +86,9 @@ while getopts ":n:h:a:k:" options; do
       ;;
     a )
       ANSIBLE=${OPTARG}
+      ;;
+    u )
+      USER=${OPTARG}
       ;;
     k )
       KEY=${OPTARG}
@@ -90,13 +100,13 @@ while getopts ":n:h:a:k:" options; do
 done
 
 
-if [[ $NAME == "" ]] | [[ $HOSTS == "" ]] | [[ $ANSIBLE == "" ]] | [[ $KEY == "" ]]; then
+if [[ $NAME == "" ]] | [[ $HOSTS == "" ]] | [[ $ANSIBLE == "" ]] | [[ $USER == "" ]] | [[ $KEY == "" ]]; then
     exit_abnormal
 fi
 
 delete_containers "$NAME"
 run_ansible_script "$ANSIBLE" "$ANSIBLE_CMD" "$HOSTS" "$ANSIBLE_SCRIPT"
-add_public_key "$NAME" "$KEY" "$SSH_FOLDER" "$AUTHORIZED_KEYS_FILE"
+add_public_key "$NAME" "$USER" "$KEY" "$SSH_FOLDER" "$AUTHORIZED_KEYS_FILE"
 
 
 
