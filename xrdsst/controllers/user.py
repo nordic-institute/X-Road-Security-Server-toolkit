b8e18d7ca0906aa7fe40a45c7555b499fb0d1477
bertvi <bert.viikmae@gofore.com>
2021-04-07 09:46:42 +0300

    pr updates

--- xrdsst/controllers/user.py
+++ xrdsst/controllers/user.py
@@ -1,7 +1,5 @@
-import logging
 import os
 import subprocess
-import sys
 
 from cement import ex
 from xrdsst.controllers.base import BaseController
@@ -53,110 +51,115 @@ class UserController(BaseController):
                     elif data.lower().find("centos") >= 0:
                         return "Centos"
                     else:
-                        return None
+                        raise Exception("Failed to determine OS as Ubuntu or CentOS, only these operation systems are supported")
                 else:
-                    self.log_error('UserController->create_user:', 'Determination of operating system for ' \
-                                       + security_server['name'] + ' failed (exit_code =' + str(exitcode) + ', data =' + str(data))
+                    raise Exception('UserController->create_user: Determination of operating system for '
+                                    + security_server['name'] + ' failed (exit_code =' + str(exitcode) + ', data =' + str(data))
             except Exception as err:
-                self.log_error('UserController->create_user:', err)
+                raise Exception('UserController->create_user:', err)
         else:
             raise Exception("SSH private key file does not exists")
 
     def prepare_os(self, user_name, os_name, ssh_key, ssh_user, security_server):
         self.log_debug('Operation system for security server \'' + security_server['name'] + '\' is: ' + os_name)
         if os_name == "Centos":
-            prepare_os = False
             cmd = "getent passwd \"xroad\""
-            check_xroad_user_cmd = 'ssh -o IdentitiesOnly=yes -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no -o LogLevel=ERROR -i "{}" {}@{} "{}"'.format(
-                ssh_key, ssh_user, self.security_server_address(security_server), cmd)
+            check_xroad_user_cmd = 'ssh -o IdentitiesOnly=yes -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no ' \
+                                   + '-o LogLevel=ERROR -i "{}" {}@{} "{}"'.format(ssh_key, ssh_user, self.security_server_address(security_server), cmd)
             exitcode, data = subprocess.getstatusoutput(check_xroad_user_cmd)
             if exitcode == 0:
                 prepare_os = False if len(data) > 0 else True
             else:
-                self.log_error('UserController->create_user:', 'Preparation procedures for adding new user on Centos for ' \
-                                   + security_server['name'] + ' failed (exit_code =' + str(exitcode) + ', data =' + str(data))
+                raise Exception('UserController->create_user: Preparation procedures for adding new user on Centos for '
+                                + security_server['name'] + ' failed (exit_code =' + str(exitcode) + ', data =' + str(data))
 
             if prepare_os:
                 self.log_debug(
                     'Performing preparation procedures before adding new user on Centos operation system for security server: ' + security_server['name'])
                 cmd = "sudo useradd --system --home /var/lib/xroad --no-create-home --shell /bin/bash --user-group --comment \"X-Road system user\" xroad"
-                add_system_user_cmd = 'ssh -o IdentitiesOnly=yes -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no -o LogLevel=ERROR -i "{}" {}@{} "{}"'.format(
-                    ssh_key, ssh_user, self.security_server_address(security_server), cmd)
+                add_system_user_cmd = 'ssh -o IdentitiesOnly=yes -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no -o LogLevel=ERROR -i "{}" {}@{} ' \
+                                      '"{}"'.format(ssh_key, ssh_user, self.security_server_address(security_server), cmd)
                 exitcode, data = subprocess.getstatusoutput(add_system_user_cmd)
                 if exitcode != 0:
-                    self.log_error('UserController->create_user:', 'Preparation procedures for adding new user on Centos for ' \
-                                       + security_server['name'] + ' failed (exit_code =' + str(exitcode) + ', data =' + str(data))
+                    raise Exception('UserController->create_user: Preparation procedures for adding new user on Centos for '
+                                    + security_server['name'] + ' failed (exit_code =' + str(exitcode) + ', data =' + str(data))
 
                 mkdir_cmd = 'ssh -o IdentitiesOnly=yes -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no -o LogLevel=ERROR -i "{}" {}@{} "{}"'.format(
                     ssh_key, ssh_user, self.security_server_address(security_server), "mkdir /etc/xroad")
                 exitcode, data = subprocess.getstatusoutput(mkdir_cmd)
                 if exitcode != 0:
-                    self.log_error('UserController->create_user:', 'Preparation procedures for adding new user on Centos for ' \
-                                       + security_server['name'] + ' failed (exit_code =' + str(exitcode) + ', data =' + str(data))
+                    raise Exception('UserController->create_user: Preparation procedures for adding new user on Centos for '
+                                    + security_server['name'] + ' failed (exit_code =' + str(exitcode) + ', data =' + str(data))
 
                 chown_cmd = 'ssh -o IdentitiesOnly=yes -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no -o LogLevel=ERROR -i "{}" {}@{} "{}"'.format(
                     ssh_key, ssh_user, self.security_server_address(security_server), "chown xroad:xroad /etc/xroad")
                 exitcode, data = subprocess.getstatusoutput(chown_cmd)
                 if exitcode != 0:
-                    self.log_error('UserController->create_user:', 'Preparation procedures for adding new user on Centos for ' \
-                                       + security_server['name'] + ' failed (exit_code =' + str(exitcode) + ', data =' + str(data))
+                    raise Exception('UserController->create_user: Preparation procedures for adding new user on Centos for '
+                                    + security_server['name'] + ' failed (exit_code =' + str(exitcode) + ', data =' + str(data))
 
                 chmod_cmd = 'ssh -o IdentitiesOnly=yes -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no -o LogLevel=ERROR -i "{}" {}@{} "{}"'.format(
                     ssh_key, ssh_user, self.security_server_address(security_server), "chmod 751 /etc/xroad")
                 exitcode, data = subprocess.getstatusoutput(chmod_cmd)
                 if exitcode != 0:
-                    self.log_error('UserController->create_user:', 'Preparation procedures for adding new user on Centos for ' \
-                                       + security_server['name'] + ' failed (exit_code =' + str(exitcode) + ', data =' + str(data))
+                    raise Exception('UserController->create_user: Preparation procedures for adding new user on Centos for '
+                                    + security_server['name'] + ' failed (exit_code =' + str(exitcode) + ', data =' + str(data))
 
                 touch_cmd = 'ssh -o IdentitiesOnly=yes -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no -o LogLevel=ERROR -i "{}" {}@{} "{}"'.format(
                     ssh_key, ssh_user, self.security_server_address(security_server), "touch /etc/xroad/db.properties")
                 exitcode, data = subprocess.getstatusoutput(touch_cmd)
                 if exitcode != 0:
-                    self.log_error('UserController->create_user:', 'Preparation procedures for adding new user on Centos for ' \
-                                       + security_server['name'] + ' failed (exit_code =' + str(exitcode) + ', data =' + str(data))
+                    raise Exception('UserController->create_user: Preparation procedures for adding new user on Centos for '
+                                    + security_server['name'] + ' failed (exit_code =' + str(exitcode) + ', data =' + str(data))
 
                 chown_cmd = 'ssh -o IdentitiesOnly=yes -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no -o LogLevel=ERROR -i "{}" {}@{} "{}"'.format(
                     ssh_key, ssh_user, self.security_server_address(security_server), "chown xroad:xroad /etc/xroad/db.properties")
                 exitcode, data = subprocess.getstatusoutput(chown_cmd)
                 if exitcode != 0:
-                    self.log_error('UserController->create_user:', 'Preparation procedures for adding new user on Centos for ' \
-                                       + security_server['name'] + ' failed (exit_code =' + str(exitcode) + ', data =' + str(data))
+                    raise Exception('UserController->create_user: Preparation procedures for adding new user on Centos for '
+                                    + security_server['name'] + ' failed (exit_code =' + str(exitcode) + ', data =' + str(data))
 
                 chmod_cmd = 'ssh -o IdentitiesOnly=yes -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no -o LogLevel=ERROR -i "{}" {}@{} "{}"'.format(
                     ssh_key, ssh_user, self.security_server_address(security_server), "chmod 640 /etc/xroad/db.properties")
                 exitcode, data = subprocess.getstatusoutput(chmod_cmd)
                 if exitcode != 0:
-                    self.log_error('UserController->create_user:', 'Preparation procedures for adding new user on Centos for ' \
-                                       + security_server['name'] + ' failed (exit_code =' + str(exitcode) + ', data =' + str(data))
+                    raise Exception('UserController->create_user: Preparation procedures for adding new user on Centos for '
+                                    + security_server['name'] + ' failed (exit_code =' + str(exitcode) + ', data =' + str(data))
 
                 cmd = "sudo groupadd -f {}".format(user_name)
-                group_add_cmd = 'ssh -o IdentitiesOnly=yes -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no -o LogLevel=ERROR -i "{}" {}@{} "{}"'.format(
-                    ssh_key, ssh_user, self.security_server_address(security_server), cmd)
+                group_add_cmd = 'ssh -o IdentitiesOnly=yes -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no ' \
+                                + '-o LogLevel=ERROR -i "{}" {}@{} "{}"'.format(ssh_key,
+                                                                                ssh_user,
+                                                                                self.security_server_address(security_server),
+                                                                                cmd)
                 exitcode, data = subprocess.getstatusoutput(group_add_cmd)
                 if exitcode != 0:
-                    self.log_error('UserController->create_user:', 'Preparation procedures for adding new user on Centos for ' \
-                                       + security_server['name'] + ' failed (exit_code =' + str(exitcode) + ', data =' + str(data))
+                    raise Exception('UserController->create_user: Preparation procedures for adding new user on Centos for '
+                                    + security_server['name'] + ' failed (exit_code =' + str(exitcode) + ', data =' + str(data))
 
                 chgrp_cmd = 'ssh -o IdentitiesOnly=yes -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no -o LogLevel=ERROR -i "{}" {}@{} "{}"'.format(
                     ssh_key, ssh_user, self.security_server_address(security_server), "chgrp shadow /etc/shadow")
                 exitcode, data = subprocess.getstatusoutput(chgrp_cmd)
                 if exitcode != 0:
-                    self.log_error('UserController->create_user:', 'Preparation procedures for adding new user on Centos for ' \
-                                       + security_server['name'] + ' failed (exit_code =' + str(exitcode) + ', data =' + str(data))
+                    raise Exception('UserController->create_user: Preparation procedures for adding new user on Centos for '
+                                    + security_server['name'] + ' failed (exit_code =' + str(exitcode) + ', data =' + str(data))
 
                 chmod_cmd = 'ssh -o IdentitiesOnly=yes -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no -o LogLevel=ERROR -i "{}" {}@{} "{}"'.format(
                     ssh_key, ssh_user, self.security_server_address(security_server), "chmod g+r /etc/shadow")
                 exitcode, data = subprocess.getstatusoutput(chmod_cmd)
                 if exitcode != 0:
-                    self.log_error('UserController->create_user:', 'Preparation procedures for adding new user on Centos for ' \
-                                       + security_server['name'] + ' failed (exit_code =' + str(exitcode) + ', data =' + str(data))
+                    raise Exception('UserController->create_user: Preparation procedures for adding new user on Centos for '
+                                    + security_server['name'] + ' failed (exit_code =' + str(exitcode) + ', data =' + str(data))
 
-                usermod_cmd = 'ssh -o IdentitiesOnly=yes -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no -o LogLevel=ERROR -i "{}" {}@{} "{}"'.format(
-                    ssh_key, ssh_user, self.security_server_address(security_server), "usermod -a -G shadow xroad || true")
+                usermod_cmd = 'ssh -o IdentitiesOnly=yes -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no ' \
+                              + '-o LogLevel=ERROR -i "{}" {}@{} "{}"'.format(ssh_key,
+                                                                              ssh_user,
+                                                                              self.security_server_address(security_server),
+                                                                              "usermod -a -G shadow xroad || true")
                 exitcode, data = subprocess.getstatusoutput(usermod_cmd)
                 if exitcode != 0:
-                    self.log_error('UserController->create_user:', 'Preparation procedures for adding new user on Centos for ' \
-                                       + security_server['name'] + ' failed (exit_code =' + str(exitcode) + ', data =' + str(data))
+                    raise Exception('UserController->create_user: Preparation procedures for adding new user on Centos for '
+                                    + security_server['name'] + ' failed (exit_code =' + str(exitcode) + ', data =' + str(data))
 
     def add_groups(self, groups, user_name, ssh_key, ssh_user, security_server):
         self.log_debug('Adding groups \'' + str(groups) + '\' for security server: ' + security_server['name'])
@@ -165,16 +168,16 @@ class UserController(BaseController):
             ssh_key, ssh_user, self.security_server_address(security_server), cmd)
         exitcode, data = subprocess.getstatusoutput(add_group_cmd)
         if exitcode != 0:
-            self.log_error('UserController->create_user:', 'Adding users own user group for ' \
-                               + security_server['name'] + ' failed (exit_code =' + str(exitcode) + ', data =' + str(data))
+            raise Exception('UserController->create_user: Adding users own user group for '
+                            + security_server['name'] + ' failed (exit_code =' + str(exitcode) + ', data =' + str(data))
         for group in groups:
             cmd = "sudo groupadd -f --system {}".format(group)
             add_group_cmd = 'ssh -o IdentitiesOnly=yes -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no -o LogLevel=ERROR -i "{}" {}@{} "{}"'.format(
                 ssh_key, ssh_user, self.security_server_address(security_server), cmd)
             exitcode, data = subprocess.getstatusoutput(add_group_cmd)
             if exitcode != 0:
-                self.log_error('UserController->create_user:', 'Adding new user groups for ' \
-                                   + security_server['name'] + ' failed (exit_code =' + str(exitcode) + ', data =' + str(data))
+                raise Exception('UserController->create_user: Adding new user groups for '
+                                + security_server['name'] + ' failed (exit_code =' + str(exitcode) + ', data =' + str(data))
 
     def add_user_with_groups(self, groups, user_name, pwd, ssh_key, ssh_user, security_server):
         self.log_debug('Adding user \'' + user_name + '\' into groups \'' + str(groups) + '\' for security server: ' + security_server['name'])
@@ -185,17 +188,15 @@ class UserController(BaseController):
         if exitcode == 0:
             for group in groups:
                 cmd = "sudo usermod -a -G {} {}".format(group, user_name)
-                user_mod_cmd = 'ssh -o IdentitiesOnly=yes -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no -o LogLevel=ERROR -i "{}" {}@{} "{}"'.format(
-                    ssh_key, ssh_user, self.security_server_address(security_server), cmd)
+                user_mod_cmd = 'ssh -o IdentitiesOnly=yes -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no ' \
+                               + '-o LogLevel=ERROR -i "{}" {}@{} "{}"'.format(ssh_key,
+                                                                               ssh_user,
+                                                                               self.security_server_address(security_server),
+                                                                               cmd)
                 exitcode, data = subprocess.getstatusoutput(user_mod_cmd)
                 if exitcode != 0:
-                    self.log_error('UserController->create_user:', 'Adding user to group for ' \
-                                       + security_server['name'] + ' failed (exit_code =' + str(exitcode) + ', data =' + str(data))
+                    raise Exception('UserController->create_user: Adding user to group for '
+                                    + security_server['name'] + ' failed (exit_code =' + str(exitcode) + ', data =' + str(data))
         else:
-            self.log_error('UserController->create_user:', 'Adding new user for ' \
-                               + security_server['name'] + ' failed (exit_code =' + str(exitcode) + ', data =' + str(data))
-
-    @staticmethod
-    def log_error(msg, exception):
-        logging.error("Exception calling " + msg + ": " + str(exception))
-        print("Exception calling " + msg + ": " + str(exception), file=sys.stderr)
+            raise Exception('UserController->create_user: Adding new user for '
+                            + security_server['name'] + ' failed (exit_code =' + str(exitcode) + ', data =' + str(data))
