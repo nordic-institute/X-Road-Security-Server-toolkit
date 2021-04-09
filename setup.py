6c8b9094986a04db8a2ce2b9d95c166710817ec3
bertvi <bert.viikmae@gofore.com>
2021-04-09 20:05:04 +0300

    refacto

--- setup.py
+++ setup.py
@@ -1,4 +1,5 @@
 """Project setup"""
+import setuptools
 from setuptools import setup, find_packages
 from xrdsst.core.version import get_version
 
@@ -18,32 +19,13 @@ setup(
     long_description=LONG_DESCRIPTION,
     long_description_content_type='text/markdown',
     classifiers=[],
-    install_requires=[
-        'cement==3.0.4',
-        'pyyaml~=5.3.1',
-        'networkx~=2.5',
-        'tabulate~=0.8.7',
-        'setuptools~=45.2.0',
-        'urllib3~=1.25.8',
-        'confuse~=1.3.0',
-        'certifi >= 14.05.14',
-        'six >= 1.10',
-        'pytest~=6.1.1',
-        'pylint~=2.6.0',
-        'pytest-pylint~=0.18.0',
-        'bump2version~=1.0.1',
-        'gitpython~=3.1.11',
-        'docker~=4.1.0',
-        'yq~=2.11.1',
-        'jq~=1.1.1',
-        'requests~=2.22.0',
-        'python-dateutil~=2.8.1'
-    ],
+    #install_requires=required,
     author='Finnish Digital Agency',
     author_email='info@dvv.fi',
     url='https://github.com/nordic-institute/X-Road-Security-Server-toolkit',
     license='MIT',
-    packages=find_packages(exclude=['ez_setup', 'tests*']),
+    #packages=find_packages(exclude=['ez_setup', 'tests*']),
+    packages=setuptools.find_packages(),
     package_data={'xrdsst': ['templates/*']},
     setup_requires=['pytest-runner', 'pytest-pylint'],
     tests_require=['pytest', 'pylint'],
