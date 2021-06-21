93e189325352f505444fb5aa1ca5baaec5e1e6e2
bertvi <bert.viikmae@gofore.com>
2021-06-21 09:31:41 +0300

    add member list sub-command

--- xrdsst/controllers/member.py
+++ xrdsst/controllers/member.py
@@ -1,5 +1,5 @@
 from cement import ex
-from xrdsst.api import MemberNamesApi
+from xrdsst.api import MemberNamesApi, MemberClassesApi, XroadInstancesApi
 from xrdsst.api_client.api_client import ApiClient
 from xrdsst.controllers.base import BaseController
 from xrdsst.resources.texts import texts
@@ -23,6 +23,21 @@ class MemberNameListMapper:
             'member_code': member.get('member_code')
         }
 
+class MemberClassListMapper:
+    @staticmethod
+    def headers():
+        return ['INSTANCE', 'MEMBER-CLASS']
+
+    @staticmethod
+    def as_list(member):
+        return [member.get('instance'), member.get('member_class')]
+
+    @staticmethod
+    def as_object(member):
+        return {
+            'instance': member.get('instance'),
+            'member_class': member.get('member_class')
+        }
 
 class MemberController(BaseController):
     class Meta:
@@ -49,6 +64,16 @@ class MemberController(BaseController):
 
         self.find_name(active_config, self.app.pargs.mclass, self.app.pargs.mcode)
 
+    @ex(help="List member classes", arguments=[(['--instance'], {'help': 'X-Road instance', 'dest': 'instance'})])
+    def list_classes(self):
+        active_config = self.load_config()
+
+        if self.app.pargs.instance is None:
+            self.log_info('X-Road instance parameter is required for listing member classes')
+            return
+
+        self.list_member_classes(active_config, self.app.pargs.instance)
+
     def find_name(self, config, member_class, member_code):
         ss_api_conf_tuple = list(zip(config["security_server"], map(lambda ss: self.create_api_config(ss, config), config["security_server"])))
 
@@ -70,5 +95,31 @@ class MemberController(BaseController):
                 self.render(render_data)
             except ApiException as err:
                 BaseController.log_api_error('MemberNamesApi->find_member_name', err)
+
+        BaseController.log_keyless_servers(ss_api_conf_tuple)
+
+    def list_member_classes(self, config, instance):
+        ss_api_conf_tuple = list(zip(config["security_server"], map(lambda ss: self.create_api_config(ss, config), config["security_server"])))
+
+        for security_server in config["security_server"]:
+            ss_api_config = self.create_api_config(security_server, config)
+            member_classes_api = MemberClassesApi(ApiClient(ss_api_config))
+            xroad_instances_api = XroadInstancesApi(ApiClient(ss_api_config))
+            try:
+                member_classes = member_classes_api.get_member_classes(current_instance=False)
+                instances = xroad_instances_api.get_xroad_instances()
+                print(instances)
+                member_class_list = []
+                for member_class in member_classes:
+                    member_class_list.append({'instance': instance, 'member_class': member_class})
+                render_data = []
+                if self.is_output_tabulated():
+                     render_data = [MemberClassListMapper.headers()]
+                     render_data.extend(map(MemberClassListMapper.as_list, member_class_list))
+                else:
+                    render_data.extend(map(MemberClassListMapper.as_object, member_class_list))
+                self.render(render_data)
+            except ApiException as err:
+                BaseController.log_api_error('MemberClassesApi->get_member_classes', err)
 
         BaseController.log_keyless_servers(ss_api_conf_tuple)
