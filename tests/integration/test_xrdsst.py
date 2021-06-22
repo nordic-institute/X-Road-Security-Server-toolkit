3c0628e639c80afcee23dd0d4b3665cf406df72d
bertvi <bert.viikmae@gofore.com>
2021-06-22 14:22:42 +0300

    refacto tests

--- tests/integration/test_xrdsst.py
+++ tests/integration/test_xrdsst.py
@@ -835,7 +835,7 @@ class TestXRDSST(IntegrationTestBase, IntegrationOpBase):
         unconfigured_servers_at_start = self.query_status()
 
         self.query_status()
-        self.step_member_find()
+        # self.step_member_find()
         self.step_member_list_classes()
         self.step_upload_anchor_fail_file_missing()
         self.step_upload_anchor_fail_file_bogus_content()
