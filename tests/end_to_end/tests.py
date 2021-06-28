38644d0a0c58c3b32cda6cb9536824c90070afaf
Alberto <alberto.fernandez@gofore.com>
2021-06-28 11:12:26 +0200

    SSTOOLKIT-90

--- tests/end_to_end/tests.py
+++ tests/end_to_end/tests.py
@@ -46,6 +46,8 @@ class EndToEndTest(unittest.TestCase):
             base = BaseController()
             base.app = app
             self.config = base.load_config(baseconfig=self.config_file)
+            print(self.config)
+
             ssn = 0
             for security_server in self.config["security_server"]:
                 if security_server.get(ConfKeysSecurityServer.CONF_KEY_API_KEY):
