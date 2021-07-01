169189b4dcc9bd3b5757b3e928d493d039a20ef9
bertvi <bert.viikmae@gofore.com>
2021-07-01 12:55:16 +0300

    fix sonar issues

--- tests/integration/test_xrdsst.py
+++ tests/integration/test_xrdsst.py
@@ -297,26 +297,20 @@ class TestXRDSST(IntegrationTestBase, IntegrationOpBase):
 
                 auth_key_label = security_server['name'] + '-default-auth-key'
                 sign_key_label = security_server['name'] + '-default-sign-key'
-                token_controller.remote_token_add_keys_with_csrs(configuration, security_server, KeyTypes.ALL,
-                                                                 member_class, member_code, member_name,
-                                                                 auth_key_label, sign_key_label)
-                if "clients" in security_server:
-                    for client in security_server["clients"]:
-                        if client["member_class"] != security_server["owner_member_class"] or client["member_code"] != \
-                                security_server["owner_member_code"]:
-                            new_member_class = client[ConfKeysSecServerClients.CONF_KEY_SS_CLIENT_MEMBER_CLASS]
-                            new_member_code = client[ConfKeysSecServerClients.CONF_KEY_SS_CLIENT_MEMBER_CODE]
-                            new_member_name = client[ConfKeysSecServerClients.CONF_KEY_SS_CLIENT_MEMBER_NAME]
-
-                            auth_key_label_new_member = security_server['name'] + '-default-auth-key_new_member'
-                            sign_key_label_new_member = security_server['name'] + '-default-sign-key_new_member'
-
-                            token_controller.remote_token_add_keys_with_csrs(configuration, security_server,
-                                                                             KeyTypes.SIGN,
-                                                                             new_member_class, new_member_code,
-                                                                             new_member_name,
-                                                                             auth_key_label_new_member,
-                                                                             sign_key_label_new_member)
+                token_controller.remote_token_add_all_keys_with_csrs(configuration,
+                                                                     security_server,
+                                                                     member_class,
+                                                                     member_code,
+                                                                     member_name,
+                                                                     auth_key_label,
+                                                                     sign_key_label)
+                for client in security_server["clients"]:
+                    if ConfKeysSecServerClients.CONF_KEY_SS_CLIENT_SUBSYSTEM_CODE not in client:
+                        token_controller.remote_token_add_sign_keys_with_csrs(configuration,
+                                                                              security_server,
+                                                                              False,
+                                                                              client,
+                                                                              auth_key_label)
                 response = token_controller.remote_get_tokens(configuration)
                 assert len(response) > 0
                 assert len(response[0].keys) == 3
