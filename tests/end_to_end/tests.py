0a2fa0616df002ed54e22f857a15eae7bc7a2af1
Bert Viikmäe <59827771+bertvi@users.noreply.github.com>
2021-07-13 15:48:49 +0300

    Update tests.py

--- tests/end_to_end/tests.py
+++ tests/end_to_end/tests.py
@@ -1026,7 +1026,7 @@ class EndToEndTest(unittest.TestCase):
                         description = get_service_descriptions(self.config, client_id, ssn)
                         assert len(description) == 2
                         response = service_controller.remote_list_access_for_services(configuration, security_server, client_id, [description[0]["id"]])
-                        assert len(response) == 1
+                        assert len(response) == 4
                         assert response[0]["security_server"] == security_server["name"]
                         assert response[0]["client_id"] == 'DEV:ORG:111:BUS'
                         assert response[0]["service_id"] == 'DEV:ORG:111:BUS:Petstore'
