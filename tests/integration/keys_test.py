472e7c52efe71b87905dcd7357551b659156a634
Alberto <alberto.fernandez@gofore.com>
2021-07-30 10:59:37 +0200

    SSTOOLKIT-129

--- tests/integration/keys_test.py
+++ tests/integration/keys_test.py
@@ -1,12 +1,36 @@
 from tests.util.test_util import get_token
 from xrdsst.main import XRDSSTTest
 from xrdsst.controllers.key import KeyController, KeyListMapper
-
+from xrdsst.controllers.token import TokenController
+from xrdsst.core.conf_keys import ConfKeysSecurityServer
 
 class KeysTest:
 
+    KEY_LABEL_AUTH = "Test_key_label_auth"
+    KEY_LABEL_SIGN = "Test_key_label_sign"
+
     def __init__(self, end_to_end_tests):
         self.test = end_to_end_tests
+
+
+
+    def step_create_test_csr(self):
+        with XRDSSTTest() as app:
+            token_controller = TokenController()
+            token_controller.app = app
+            for security_server in self.test.config["security_server"]:
+                configuration = token_controller.create_api_config(security_server, self.test.config)
+                member_class = security_server[ConfKeysSecurityServer.CONF_KEY_MEMBER_CLASS]
+                member_code = security_server[ConfKeysSecurityServer.CONF_KEY_MEMBER_CODE]
+                member_name = security_server[ConfKeysSecurityServer.CONF_KEY_DN_ORG]
+
+                token_controller.remote_token_add_all_keys_with_csrs(configuration,
+                                                                     security_server,
+                                                                     member_class,
+                                                                     member_code,
+                                                                     member_name,
+                                                                     self.KEY_LABEL_AUTH,
+                                                                     self.sign_key_label)
 
     def step_key_list(self):
         list_key_dic = []
@@ -56,17 +80,18 @@ class KeysTest:
             key_controller.app = app
             key_controller.load_config = (lambda: self.test.config)
             for security_server in self.test.config["security_server"]:
-                key_list = list(filter(lambda k: k["ss_name"] == security_server["name"], key_list_dic))[0]["keys"]
-                key_to_delete = list(filter(lambda k: k["usage"] == "SIGNING", key_list))[0]
                 configuration = key_controller.create_api_config(security_server, self.test.config)
-                key_controller.remote_delete_keys(configuration, security_server["name"], [key_to_delete["id"]])
-
+                key_list = list(filter(lambda k: k["ss_name"] == security_server["name"], key_list_dic))[0]["keys"]
+                keys_to_delete = list(filter(lambda k: k["label"] == self.KEY_LABEL_AUTH or k["label"] == self.KEY_LABEL_SIGN, key_list))
+                id_keys_to_delete = (k["id"] for k in keys_to_delete)
+                key_controller.remote_delete_keys(configuration, security_server["name"], id_keys_to_delete)
                 key_list_after = key_controller.remote_list_keys(configuration, "0")
 
-                assert len(key_list_after) == (len(key_list) - 1)
+                assert len(key_list_after) == (len(key_list) - 2)
 
     def test_run_configuration(self):
         key_list_dic = self.step_key_list()
+        self.step_create_test_csr()
         self.step_key_update(key_list_dic)
-        
+
         # self.step_key_delete(key_list_dic)
